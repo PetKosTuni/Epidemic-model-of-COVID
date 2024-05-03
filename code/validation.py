e37c93a4c66e5f8a9276f12be0fa126bc197ecbd
Petri Kosonen <petri.kosonen@tuni.fi>
2024-05-03 19:02:49 +0300

    Bug fixes for file names

--- code/validation.py
+++ code/validation.py
@@ -123,7 +123,7 @@ def get_region_list():
             data = DATASET_template(args.dataset_filepath, pdata.custom_dataset_columns, level = 'states')
         else:
             data = JHU_US(level='states')
-            args.dataset = 'JHU_US'
+            args.dataset = 'JHU'
 
         nonstate_list = ["American Samoa", "Diamond Princess", "Grand Princess", "Virgin Islands", "Northern Mariana Islands"]
         region_list = [state for state in data.state_list if state not in nonstate_list]
@@ -142,7 +142,7 @@ def get_region_list():
             data = DATASET_template(args.dataset_filepath, pdata.custom_dataset_columns, level = 'counties')
         else:
             data = JHU_US(level='counties')
-            args.dataset = 'JHU_US'
+            args.dataset = 'JHU'
         
         mid_dates = pdata.mid_dates_county
         with open("data/county_pop.json", 'r') as f:
@@ -163,7 +163,7 @@ def get_region_list():
             data = DATASET_template(args.dataset_filepath, pdata.custom_dataset_columns, level = 'nation')
         else:
             data = JHU_global()
-            args.dataset = 'JHU_global'
+            args.dataset = 'JHU'
 
         region_list = pdata.START_nation.keys()
         mid_dates = pdata.mid_dates_nation
